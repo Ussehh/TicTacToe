9767a184c9bd2033642baa17c65dcc6954ae9eb2
Ussehh <nikita.abaev@icloud.com>
2024-04-22 18:54:40 +0500

    Initial Commit

---
+++ TicTacToe/TicTacToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacToeApp.swift
+//  TicTacToe
+//
+//  Created by Никита Абаев on 22.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct TicTacToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
